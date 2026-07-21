c362a833bc895fffed5049631bc9391a551f5345
morita-y9 <xiongxisentian@gmail.com>
2026-07-21 15:44:08 +0900

    link_budget

--- linkbudget_LEO.m
+++ linkbudget_LEO.m
@@ -1,44 +1,41 @@
-clear; clc;
-
 Pt_mW    = 20;
 Gt       = 1.0;
 Gr0      = 6.0;
+NR       = 9;
 NF       = 4.0;
-HPBW     = 80;
-fontSize = 14;
+T0       = 290;
+BW       = 100e3;
+fc       = 920e6;
+h        = 540e3;
+Re       = 6.371e6;
+GM       = 3.986e14;
+c_light  = 3e8;
+k_dB     = -228.6;
+fontSize = 24０;
 
-c_light = 3e8;
-GM  = 3.986e14;
-Re  = 6.371e6;
-fc  = 920e6;
-h   = 540e3;
-BW  = 100e3;
-T0  = 290;
-k_dB = -228.6;
+HPBW     = 80;
+L_margin = 6.0;
 
 v_sat  = sqrt(GM/(Re+h));
 Pt_dBW = 10*log10(Pt_mW/1000);
 EIRP   = Pt_dBW + Gt;
-
 El   = linspace(1, 90, 500);
 El_r = El * pi/180;
-
 d_m    = sqrt((Re+h)^2 - (Re*cos(El_r)).^2) - Re*sin(El_r);
 lambda = c_light/fc;
 FSPL   = 20*log10(4*pi*d_m/lambda);
-
 gamma_sat = asin(Re * cos(El_r) / (Re+h));
 L_scan    = 12 * (gamma_sat*180/pi / HPBW).^2;
-Gr_eff    = Gr0 - L_scan;
-
+Gr_array  = Gr0 + 10*log10(NR);
+Gr_eff    = Gr_array - L_scan;
 T_sys = T0 * 10^(NF/10);
 GT    = Gr_eff - 10*log10(T_sys);
-
-CNo = EIRP - FSPL + GT - k_dB;
+CNo = EIRP - FSPL + GT - k_dB - L_margin;
 
 SF_list = [7 10 12];
-SNR_req = [-7.5 -12.5 -15 -17.5 -20];
+SNR_req = [-7.5 -15 -20];
 CNo_req = SNR_req + 10*log10(BW);
+
 
 figure;
 colors = lines(length(SF_list));
